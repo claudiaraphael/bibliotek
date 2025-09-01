fb97377ac3680c3e8d6f34ed01d5c3d14d49878b
Claudia Rodrigues <50813092+claudiaraphael@users.noreply.github.com>
2025-09-01 03:51:44 -0300

    Atualizar o main.py com comentarios para documentar o aprendizado. Assim ficara mais facil fazer os posts do linkedin.

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 
 Base.metadata.create_all(bind=engine)
 
+# o primeiro endpoint da API é o root 
+
+# seria o msm root da DOM q aparece no CSS?
+
 @app.get("/")
 def root():
     return {"message": "Hello World"}
